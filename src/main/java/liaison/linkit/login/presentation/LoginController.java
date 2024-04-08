c8a23466d8a5574ce1daaade518041f2b69b42b6
oznchex <114240463+oznchex@users.noreply.github.com>
2024-04-08 09:39:21 +0900

    [Feat] Login

--- src/main/java/liaison/linkit/login/presentation/LoginController.java
+++ src/main/java/liaison/linkit/login/presentation/LoginController.java
@@ -1,6 +1,9 @@
 package liaison.linkit.login.presentation;
 
 import jakarta.servlet.http.HttpServletResponse;
+import liaison.linkit.auth.Auth;
+import liaison.linkit.auth.MemberOnly;
+import liaison.linkit.auth.domain.Accessor;
 import liaison.linkit.login.domain.MemberTokens;
 import liaison.linkit.login.dto.AccessTokenResponse;
 import liaison.linkit.login.dto.LoginRequest;
@@ -8,10 +11,7 @@ import liaison.linkit.login.service.LoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpHeaders.SET_COOKIE;
 import static org.springframework.http.HttpStatus.CREATED;
@@ -39,4 +39,29 @@ public class LoginController {
         response.addHeader(SET_COOKIE, cookie.toString());
         return ResponseEntity.status(CREATED).body(new AccessTokenResponse(memberTokens.getAccessToken()));
     }
+
+    @PostMapping("/token")
+    public ResponseEntity<AccessTokenResponse> extendLogin(
+            @CookieValue("refresh-token") final String refreshToken,
+            @RequestHeader("Authorization") final String authorizationHeader
+    ) {
+        final String renewalRefreshToken = loginService.renewalAccessToken(refreshToken, authorizationHeader);
+        return ResponseEntity.status(CREATED).body(new AccessTokenResponse(renewalRefreshToken));
+    }
+
+    @DeleteMapping("/logout")
+    @MemberOnly
+    public ResponseEntity<Void> logout(
+            @Auth final Accessor accessor,
+            @CookieValue("refresh-token") final String refreshToken) {
+        loginService.removeRefreshToken(refreshToken);
+        return ResponseEntity.noContent().build();
+    }
+
+//    @DeleteMapping("/account")
+//    @MemberOnly
+//    public ResponseEntity<Void> deleteAccount(@Auth final Accessor accessor) {
+//        loginService.deleteAccount(accessor.getMemberId());
+//        return ResponseEntity.noContent().build();
+//    }
 }
