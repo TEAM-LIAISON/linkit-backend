d20b9d63548f9126bf67725f6954a85f4a23f88d
oznchex <114240463+oznchex@users.noreply.github.com>
2024-04-24 23:14:05 +0900

    [Docs] MiniProfile API 완료

---
+++ src/test/java/liaison/linkit/profile/presentation/AwardsControllerTest.java
@@ -0,0 +1,10 @@
+package liaison.linkit.profile.presentation;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(AwardsController.class)
+class AwardsControllerTest {
+
+}
