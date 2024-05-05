a24717739109da0ef58f2a374d1329736e926e15
oznchex <114240463+oznchex@users.noreply.github.com>
2024-05-05 17:13:45 +0900

    [Docs] 프로필 자기소개 항목 업데이트 API Update

--- src/test/java/liaison/linkit/profile/presentation/ProfileControllerTest.java
+++ src/test/java/liaison/linkit/profile/presentation/ProfileControllerTest.java
@@ -7,19 +7,31 @@ import liaison.linkit.login.domain.MemberTokens;
 import liaison.linkit.profile.dto.request.ProfileUpdateRequest;
 import liaison.linkit.profile.service.ProfileService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static liaison.linkit.global.restdocs.RestDocsConfiguration.field;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.restdocs.cookies.CookieDocumentation.cookieWithName;
+import static org.springframework.restdocs.cookies.CookieDocumentation.requestCookies;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProfileController.class)
 @MockBean(JpaMetamodelMappingContext.class)
@@ -43,14 +55,50 @@ class ProfileControllerTest extends ControllerTest {
         given(profileService.validateProfileByMember(1L)).willReturn(1L);
     }
 
-    private ResultActions performPutRequest(final ProfileUpdateRequest updateRequest) throws Exception {
+    private ResultActions performPatchRequest(final ProfileUpdateRequest updateRequest) throws Exception {
         return mockMvc.perform(
-                put("/profile")
+                patch("/profile")
                         .header(AUTHORIZATION, MEMBER_TOKENS.getAccessToken())
                         .cookie(COOKIE)
                         .contentType(APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(updateRequest))
         );
+    }
+
+    @DisplayName("프로필 자기소개 항목을 수정할 수 있다.")
+    @Test
+    void updateProfileIntroduction() throws Exception {
+        // given
+        final ProfileUpdateRequest updateRequest = new ProfileUpdateRequest(
+                "자기소개를 수정하려고 합니다."
+        );
+
+        doNothing().when(profileService).update(anyLong(), any(ProfileUpdateRequest.class));
+
+        // when
+        final ResultActions resultActions = performPatchRequest(updateRequest);
+
+        // then
+        resultActions.andExpect(status().isNoContent())
+                .andDo(
+                        restDocs.document(
+                                requestCookies(
+                                        cookieWithName("refresh-token")
+                                                .description("갱신 토큰")
+                                ),
+                                requestHeaders(
+                                        headerWithName("Authorization")
+                                                .description("access token")
+                                                .attributes(field("constraint", "문자열(jwt)"))
+                                ),
+                                requestFields(
+                                        fieldWithPath("introduction")
+                                                .type(JsonFieldType.STRING)
+                                                .description("자기 소개")
+                                                .attributes(field("constraint", "문자열"))
+                                )
+                        )
+                );
     }
 
 //    @DisplayName("프로필 자기소개 항목 수정할 수 있다.")
