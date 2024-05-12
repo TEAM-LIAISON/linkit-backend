2650a8b549d1b8f672d388aa40e5db5dc6b08fec
oznchex <114240463+oznchex@users.noreply.github.com>
2024-05-12 15:40:12 +0900

    [Fix] S3 error

--- src/main/java/liaison/linkit/global/config/S3Config.java
+++ src/main/java/liaison/linkit/global/config/S3Config.java
@@ -4,12 +4,15 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
+    private static final Logger log = LoggerFactory.getLogger(S3Config.class);
 
     @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
@@ -22,6 +25,8 @@ public class S3Config {
 
     @Bean
     public AmazonS3 amazonS3Client() {
+        log.info("Creating AmazonS3Client with access key: {}", accessKey);
+        log.info("Creating AmazonS3Client with secret key: {}", secretKey);
         BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
         return AmazonS3ClientBuilder.standard()
