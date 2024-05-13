91ba4b811f6382e97822e9802c8c14ab37f868a6
oznchex <114240463+oznchex@users.noreply.github.com>
2024-05-13 13:35:22 +0900

    [Login delete]

--- src/main/java/liaison/linkit/login/service/LoginService.java
+++ src/main/java/liaison/linkit/login/service/LoginService.java
@@ -1,102 +1,102 @@
-package liaison.linkit.login.service;
-
-import liaison.linkit.global.exception.AuthException;
-import liaison.linkit.login.domain.*;
-import liaison.linkit.login.domain.repository.RefreshTokenRepository;
-import liaison.linkit.login.dto.MemberTokensAndIsBasicInform;
-import liaison.linkit.login.infrastructure.BearerAuthorizationExtractor;
-import liaison.linkit.login.infrastructure.JwtProvider;
-import liaison.linkit.member.domain.Member;
-import liaison.linkit.member.domain.repository.MemberRepository;
-import liaison.linkit.profile.domain.Profile;
-import liaison.linkit.profile.domain.repository.ProfileRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import static liaison.linkit.global.exception.ExceptionCode.*;
-
-@Service
-@Transactional
-@RequiredArgsConstructor
-public class LoginService {
-
-    private static final int MAX_TRY_COUNT = 5;
-
-    private final RefreshTokenRepository refreshTokenRepository;
-
-    private final MemberRepository memberRepository;
-    private final ProfileRepository profileRepository;
-
-    private final OauthProviders oauthProviders;
-    private final JwtProvider jwtProvider;
-    private final BearerAuthorizationExtractor bearerExtractor;
-
-    public MemberTokensAndIsBasicInform login(final String providerName, final String code) {
-
-        final OauthProvider provider = oauthProviders.mapping(providerName);
-        final OauthUserInfo oauthUserInfo = provider.getUserInfo(code);
-        final Member member = findOrCreateMember(
-                oauthUserInfo.getSocialLoginId(),
-                oauthUserInfo.getEmail()
-        );
-        final boolean isMemberBasicInform = member.getIsMemberBasicInform();
-        final MemberTokens memberTokens = jwtProvider.generateLoginToken(member.getId().toString());
-        final RefreshToken savedRefreshToken = new RefreshToken(memberTokens.getRefreshToken(), member.getId());
-        refreshTokenRepository.save(savedRefreshToken);
-
-        return new MemberTokensAndIsBasicInform(
-                memberTokens.getAccessToken(), memberTokens.getRefreshToken(), isMemberBasicInform
-        );
-    }
-
-    private Member findOrCreateMember(final String socialLoginId, final String email) {
-        return memberRepository.findBySocialLoginId(socialLoginId)
-                .orElseGet(() -> createMember(socialLoginId, email));
-    }
-
-    private Member createMember(final String socialLoginId, final String email) {
-        int tryCount = 0;
-        while (tryCount < MAX_TRY_COUNT) {
-            if (!memberRepository.existsByEmail(email)) {
-                Member member = memberRepository.save(new Member(socialLoginId, email, null));
-                profileRepository.save(new Profile(member, 0,"자기소개를 입력해주세요"));
-                return member;
-            }
-            tryCount += 1;
-        }
-        throw new AuthException(FAIL_TO_GENERATE_RANDOM_NICKNAME);
-    }
-
-    public String renewalAccessToken(final String refreshTokenRequest, final String authorizationHeader) {
-        final String accessToken = bearerExtractor.extractAccessToken(authorizationHeader);
-        if (jwtProvider.isValidRefreshAndInvalidAccess(refreshTokenRequest, accessToken)) {
-            final RefreshToken refreshToken = refreshTokenRepository.findById(refreshTokenRequest)
-                    .orElseThrow(() -> new AuthException(INVALID_REFRESH_TOKEN));
-            return jwtProvider.regenerateAccessToken(refreshToken.getMemberId().toString());
-        }
-        if (jwtProvider.isValidRefreshAndValidAccess(refreshTokenRequest, accessToken)) {
-            return accessToken;
-        }
-        throw new AuthException(FAIL_TO_VALIDATE_TOKEN);
-    }
-
-    public void removeRefreshToken(final String refreshToken) {
-        refreshTokenRepository.deleteById(refreshToken);
-    }
-
-    // 수정 필요
-    public void deleteAccount(final Long memberId) {
-        memberRepository.deleteByMemberId(memberId);
-    }
-
-    public boolean getIsMemberBasicInform(final String providerName, final String code) {
-        final OauthProvider provider = oauthProviders.mapping(providerName);
-        final OauthUserInfo oauthUserInfo = provider.getUserInfo(code);
-        final Member member = findOrCreateMember(
-                oauthUserInfo.getSocialLoginId(),
-                oauthUserInfo.getEmail()
-        );
-        return member.getIsMemberBasicInform();
-    }
-}
+//package liaison.linkit.login.service;
+//
+//import liaison.linkit.global.exception.AuthException;
+//import liaison.linkit.login.domain.*;
+//import liaison.linkit.login.domain.repository.RefreshTokenRepository;
+//import liaison.linkit.login.dto.MemberTokensAndIsBasicInform;
+//import liaison.linkit.login.infrastructure.BearerAuthorizationExtractor;
+//import liaison.linkit.login.infrastructure.JwtProvider;
+//import liaison.linkit.member.domain.Member;
+//import liaison.linkit.member.domain.repository.MemberRepository;
+//import liaison.linkit.profile.domain.Profile;
+//import liaison.linkit.profile.domain.repository.ProfileRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import static liaison.linkit.global.exception.ExceptionCode.*;
+//
+//@Service
+//@Transactional
+//@RequiredArgsConstructor
+//public class LoginService {
+//
+//    private static final int MAX_TRY_COUNT = 5;
+//
+//    private final RefreshTokenRepository refreshTokenRepository;
+//
+//    private final MemberRepository memberRepository;
+//    private final ProfileRepository profileRepository;
+//
+//    private final OauthProviders oauthProviders;
+//    private final JwtProvider jwtProvider;
+//    private final BearerAuthorizationExtractor bearerExtractor;
+//
+//    public MemberTokensAndIsBasicInform login(final String providerName, final String code) {
+//
+//        final OauthProvider provider = oauthProviders.mapping(providerName);
+//        final OauthUserInfo oauthUserInfo = provider.getUserInfo(code);
+//        final Member member = findOrCreateMember(
+//                oauthUserInfo.getSocialLoginId(),
+//                oauthUserInfo.getEmail()
+//        );
+//        final boolean isMemberBasicInform = member.getIsMemberBasicInform();
+//        final MemberTokens memberTokens = jwtProvider.generateLoginToken(member.getId().toString());
+//        final RefreshToken savedRefreshToken = new RefreshToken(memberTokens.getRefreshToken(), member.getId());
+//        refreshTokenRepository.save(savedRefreshToken);
+//
+//        return new MemberTokensAndIsBasicInform(
+//                memberTokens.getAccessToken(), memberTokens.getRefreshToken(), isMemberBasicInform
+//        );
+//    }
+//
+//    private Member findOrCreateMember(final String socialLoginId, final String email) {
+//        return memberRepository.findBySocialLoginId(socialLoginId)
+//                .orElseGet(() -> createMember(socialLoginId, email));
+//    }
+//
+//    private Member createMember(final String socialLoginId, final String email) {
+//        int tryCount = 0;
+//        while (tryCount < MAX_TRY_COUNT) {
+//            if (!memberRepository.existsByEmail(email)) {
+//                Member member = memberRepository.save(new Member(socialLoginId, email, null));
+//                profileRepository.save(new Profile(member, 0,"자기소개를 입력해주세요"));
+//                return member;
+//            }
+//            tryCount += 1;
+//        }
+//        throw new AuthException(FAIL_TO_GENERATE_RANDOM_NICKNAME);
+//    }
+//
+//    public String renewalAccessToken(final String refreshTokenRequest, final String authorizationHeader) {
+//        final String accessToken = bearerExtractor.extractAccessToken(authorizationHeader);
+//        if (jwtProvider.isValidRefreshAndInvalidAccess(refreshTokenRequest, accessToken)) {
+//            final RefreshToken refreshToken = refreshTokenRepository.findById(refreshTokenRequest)
+//                    .orElseThrow(() -> new AuthException(INVALID_REFRESH_TOKEN));
+//            return jwtProvider.regenerateAccessToken(refreshToken.getMemberId().toString());
+//        }
+//        if (jwtProvider.isValidRefreshAndValidAccess(refreshTokenRequest, accessToken)) {
+//            return accessToken;
+//        }
+//        throw new AuthException(FAIL_TO_VALIDATE_TOKEN);
+//    }
+//
+//    public void removeRefreshToken(final String refreshToken) {
+//        refreshTokenRepository.deleteById(refreshToken);
+//    }
+//
+//    // 수정 필요
+//    public void deleteAccount(final Long memberId) {
+//        memberRepository.deleteByMemberId(memberId);
+//    }
+//
+//    public boolean getIsMemberBasicInform(final String providerName, final String code) {
+//        final OauthProvider provider = oauthProviders.mapping(providerName);
+//        final OauthUserInfo oauthUserInfo = provider.getUserInfo(code);
+//        final Member member = findOrCreateMember(
+//                oauthUserInfo.getSocialLoginId(),
+//                oauthUserInfo.getEmail()
+//        );
+//        return member.getIsMemberBasicInform();
+//    }
+//}
