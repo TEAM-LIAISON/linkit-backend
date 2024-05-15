61b0ad6ab033ac1cf4c5795aa201d0cdd5142ec4
oznchex <114240463+oznchex@users.noreply.github.com>
2024-05-15 23:17:03 +0900

    [Feat] Team Profile Entity

--- src/main/java/liaison/linkit/team/domain/TeamProfile.java
+++ src/main/java/liaison/linkit/team/domain/TeamProfile.java
@@ -1,4 +1,41 @@
 package liaison.linkit.team.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
 public class TeamProfile {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(name = "team_introduction")
+    private String teamIntroduction;
+
+    @Column(nullable = false)
+    private boolean isTeamTeamBuildingField;
+
+    @Column(nullable = false)
+    private boolean isTeamMemberAnnouncement;
+
+    @Column(nullable = false)
+    private boolean isActivityMethod;
+
+    @Column(nullable = false)
+    private boolean isTeamIntroduction;
+
+    @Column(nullable = false)
+    private boolean isTeamMemberIntroduction;
+
+    @Column(nullable = false)
+    private boolean isHistory;
 }
