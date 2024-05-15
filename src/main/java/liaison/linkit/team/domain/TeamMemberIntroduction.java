675605965d573ce8f19470df569dace8486b133a
oznchex <114240463+oznchex@users.noreply.github.com>
2024-05-16 00:42:07 +0900

    [Feat] Team / Member Introduction Entity

---
+++ src/main/java/liaison/linkit/team/domain/TeamMemberIntroduction.java
@@ -0,0 +1,33 @@
+package liaison.linkit.team.domain;
+
+import jakarta.persistence.*;
+import liaison.linkit.member.domain.MemberRole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+public class TeamMemberIntroduction {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "team_member_introduction_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "team_profile_id")
+    private TeamProfile teamProfile;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_role")
+    private MemberRole memberRole;
+
+    @Column(name = "member_name")
+    private String memberName;
+}
