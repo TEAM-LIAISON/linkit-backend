27b3637a69cc0aba048597e3103864c01b572dc9
oznchex <114240463+oznchex@users.noreply.github.com>
2024-05-20 20:12:21 +0900

    [Feat] MiniProfile Create 구현

--- src/main/java/liaison/linkit/profile/dto/request/miniProfile/MiniProfileCreateRequest.java
+++ src/main/java/liaison/linkit/profile/dto/request/miniProfile/MiniProfileCreateRequest.java
@@ -9,12 +9,19 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class MiniProfileCreateRequest {
 
+    // 나의 프로필 제목
     private final String profileTitle;
-    private final LocalDate uploadPeriod;
-    private final boolean uploadDeadline;
-    private final String miniProfileImg;
-    private final String myValue;
-    private final String skillSets;
 
+    // 프로필 업로드 기간
+    private final LocalDate uploadPeriod;
+
+    // 마감 선택 여부
+    private final boolean uploadDeadline;
+
+    // 협업 시 중요한 나의 가치
+    private final String myValue;
+
+    // 나의 스킬셋
+    private final String skillSets;
 
 }
--- src/main/java/liaison/linkit/profile/service/MiniProfileService.java
+++ src/main/java/liaison/linkit/profile/service/MiniProfileService.java
@@ -2,6 +2,10 @@ package liaison.linkit.profile.service;
 
 import liaison.linkit.global.exception.AuthException;
 import liaison.linkit.global.exception.BadRequestException;
+import liaison.linkit.global.exception.ImageException;
+import liaison.linkit.image.domain.ImageFile;
+import liaison.linkit.image.domain.S3ImageEvent;
+import liaison.linkit.image.domain.infrastructure.S3Uploader;
 import liaison.linkit.profile.domain.MiniProfile;
 import liaison.linkit.profile.domain.Profile;
 import liaison.linkit.profile.domain.repository.MiniProfileRepository;
@@ -10,12 +14,12 @@ import liaison.linkit.profile.dto.request.miniProfile.MiniProfileCreateRequest;
 import liaison.linkit.profile.dto.request.miniProfile.MiniProfileUpdateRequest;
 import liaison.linkit.profile.dto.response.MiniProfileResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import static liaison.linkit.global.exception.ExceptionCode.INVALID_MINI_PROFILE_WITH_MEMBER;
-import static liaison.linkit.global.exception.ExceptionCode.NOT_FOUND_MINI_PROFILE_ID;
+import static liaison.linkit.global.exception.ExceptionCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +28,8 @@ public class MiniProfileService {
 
     private final MiniProfileRepository miniProfileRepository;
     private final ProfileRepository profileRepository;
+    private final S3Uploader s3Uploader;
+    private final ApplicationEventPublisher publisher;
 
     public Long validateMiniProfileByMember(final Long memberId) {
         Long profileId = profileRepository.findByMemberId(memberId).getId();
@@ -36,15 +42,18 @@ public class MiniProfileService {
 
     public void save(final Long memberId,
                      final MiniProfileCreateRequest miniProfileCreateRequest,
-                     final MultipartFile multipartFile
+                     final MultipartFile miniProfileImage
     ) {
         final Profile profile = profileRepository.findByMemberId(memberId);
+        // 전달받은 multipartFile을 파일 경로에 맞게 전달하는 작업이 필요함 (save)
+        final String miniProfileImageUrl = saveImage(miniProfileImage);
+
         final MiniProfile newMiniProfile = MiniProfile.of(
                 profile,
                 miniProfileCreateRequest.getProfileTitle(),
                 miniProfileCreateRequest.getUploadPeriod(),
                 miniProfileCreateRequest.isUploadDeadline(),
-                miniProfileCreateRequest.getMiniProfileImg(),
+                miniProfileImageUrl,
                 miniProfileCreateRequest.getMyValue(),
                 miniProfileCreateRequest.getSkillSets()
         );
@@ -76,5 +85,29 @@ public class MiniProfileService {
             throw new BadRequestException(NOT_FOUND_MINI_PROFILE_ID);
         }
         miniProfileRepository.deleteById(miniProfileId);
+    }
+
+    public String saveImage(final MultipartFile miniProfileImage) {
+        // 이미지 유효성 검증
+        validateSizeOfImage(miniProfileImage);
+        // 이미지 파일 객체 생성 (file, HashedName)
+        final ImageFile imageFile = new ImageFile(miniProfileImage);
+        // 파일 이름을 반환한다? No! -> 파일 경로를 반환해야함.
+        return uploadMiniProfileImage(imageFile);
+    }
+
+    private String uploadMiniProfileImage(final ImageFile miniProfileImageFile) {
+        try {
+            return s3Uploader.uploadMiniProfileImage(miniProfileImageFile);
+        } catch (final ImageException e) {
+            publisher.publishEvent(new S3ImageEvent(miniProfileImageFile.getHashedName()));
+            throw e;
+        }
+    }
+
+    private void validateSizeOfImage(final MultipartFile image) {
+        if (image == null || image.isEmpty()) {
+            throw new ImageException(EMPTY_IMAGE);
+        }
     }
 }
