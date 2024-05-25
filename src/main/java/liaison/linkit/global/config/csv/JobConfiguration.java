15afdf59bfe99bb37ba3b8283ce6f2195e52b826
oznchex <114240463+oznchex@users.noreply.github.com>
2024-05-26 02:29:07 +0900

    [ Feat] Major csv auto update

--- src/main/java/liaison/linkit/global/config/csv/JobConfiguration.java
+++ src/main/java/liaison/linkit/global/config/csv/JobConfiguration.java
@@ -1,7 +1,12 @@
-package liaison.linkit.global.config.csv.university;
+package liaison.linkit.global.config.csv;
 
+import liaison.linkit.global.config.csv.major.CsvMajorReader;
+import liaison.linkit.global.config.csv.major.CsvMajorWriter;
 import liaison.linkit.global.config.csv.teamBuildingField.CsvTeamBuildingFieldReader;
 import liaison.linkit.global.config.csv.teamBuildingField.CsvTeamBuildingFieldWriter;
+import liaison.linkit.global.config.csv.university.CsvUniversityReader;
+import liaison.linkit.global.config.csv.university.CsvUniversityWriter;
+import liaison.linkit.profile.dto.csv.MajorCsvData;
 import liaison.linkit.profile.dto.csv.TeamBuildingFieldCsvData;
 import liaison.linkit.profile.dto.csv.UniversityCsvData;
 import lombok.RequiredArgsConstructor;
@@ -18,21 +23,27 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-public class UniversityJobConfiguration {
+public class JobConfiguration {
 
     private final CsvUniversityReader csvUniversityReader;
     private final CsvUniversityWriter csvUniversityWriter;
 
-
     private final CsvTeamBuildingFieldReader csvTeamBuildingFieldReader;
     private final CsvTeamBuildingFieldWriter csvTeamBuildingFieldWriter;
 
+    private final CsvMajorReader csvMajorReader;
+    private final CsvMajorWriter csvMajorWriter;
+
     // Step & Job upload
     @Bean
-    public Job universityDataLoadJob(JobRepository jobRepository, Step universityDataLoadStep, Step teamBuildingFieldDataLoadStep) {
-        return new JobBuilder("universityInformationLoadJob", jobRepository)
+    public Job simpleDataLoadJob(JobRepository jobRepository,
+                                 Step universityDataLoadStep,
+                                 Step teamBuildingFieldDataLoadStep,
+                                 Step majorDataLoadStep) {
+        return new JobBuilder("linkitInformationLoadJob", jobRepository)
                 .start(universityDataLoadStep) // 스텝 실행
                 .next(teamBuildingFieldDataLoadStep)
+                .next(majorDataLoadStep)
                 .build();
         // 필요 시 listener 추가 가능
     }
@@ -57,6 +68,19 @@ public class UniversityJobConfiguration {
                 .<TeamBuildingFieldCsvData, TeamBuildingFieldCsvData>chunk(500, platformTransactionManager)
                 .reader(csvTeamBuildingFieldReader.csvTeamBuildingFieldReader())
                 .writer(csvTeamBuildingFieldWriter)
+                .allowStartIfComplete(true)
+                .build();
+    }
+
+    @Bean
+    public Step majorDataLoadStep(
+            JobRepository jobRepository,
+            PlatformTransactionManager platformTransactionManager
+    ) {
+        return new StepBuilder("majorDataLoadStep", jobRepository)
+                .<MajorCsvData, MajorCsvData>chunk(1000, platformTransactionManager)
+                .reader(csvMajorReader.csvMajorReader())
+                .writer(csvMajorWriter)
                 .allowStartIfComplete(true)
                 .build();
     }
