eadfe87a5e771be44b2e6c222ae7d29f74c74792
oznchex <114240463+oznchex@users.noreply.github.com>
2024-06-03 06:01:16 +0900

    [HotFix] Education 등록 수정

--- src/main/java/liaison/linkit/profile/domain/education/Education.java
+++ src/main/java/liaison/linkit/profile/domain/education/Education.java
@@ -2,7 +2,7 @@ package liaison.linkit.profile.domain.education;
 
 import jakarta.persistence.*;
 import liaison.linkit.profile.domain.Profile;
-import liaison.linkit.profile.dto.request.EducationUpdateRequest;
+import liaison.linkit.profile.dto.request.education.EducationUpdateRequest;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
