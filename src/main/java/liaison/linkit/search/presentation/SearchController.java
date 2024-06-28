e5dc08e6712c52cc3871de7adbde0fd81eedec88
oznchex <114240463+oznchex@users.noreply.github.com>
2024-06-28 23:36:15 +0900

    feat : search Controller 팀원 찾기 및 팀 찾기 구현 시작

---
+++ src/main/java/liaison/linkit/search/presentation/SearchController.java
@@ -0,0 +1,18 @@
+package liaison.linkit.search.presentation;
+
+import liaison.linkit.search.service.SearchService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping
+@Slf4j
+// 팀 찾기 및 팀원 찾기 컨트롤러
+public class SearchController {
+
+    public final SearchService searchService;
+
+}
---
+++ src/main/java/liaison/linkit/search/service/SearchService.java
@@ -0,0 +1,12 @@
+package liaison.linkit.search.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class SearchService {
+
+}
