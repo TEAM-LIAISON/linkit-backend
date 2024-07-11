447f32f0131d22ba2e4f8bae118c43ed95ed54f3
oznchex <114240463+oznchex@users.noreply.github.com>
2024-07-11 18:09:54 +0900

    feat : POST antecedents education once upload

--- src/main/java/liaison/linkit/profile/presentation/EducationController.java
+++ src/main/java/liaison/linkit/profile/presentation/EducationController.java
@@ -4,10 +4,12 @@ import jakarta.validation.Valid;
 import liaison.linkit.auth.Auth;
 import liaison.linkit.auth.MemberOnly;
 import liaison.linkit.auth.domain.Accessor;
+import liaison.linkit.profile.dto.request.education.EducationCreateRequest;
 import liaison.linkit.profile.dto.request.education.EducationListCreateRequest;
 import liaison.linkit.profile.service.EducationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,16 +22,28 @@ public class EducationController {
     public final EducationService educationService;
 
     // 1.5.6. 학력 생성/수정
-    @PostMapping("/education")
+    @PostMapping("/educations")
     @MemberOnly
-    public ResponseEntity<Void> createEducation(
+    public ResponseEntity<Void> createEducations(
             @Auth final Accessor accessor,
             @RequestBody @Valid EducationListCreateRequest educationListCreateRequest
     ) {
         log.info("memberId={}의 학력 생성/수정 요청이 들어왔습니다.", accessor.getMemberId());
-        educationService.save(accessor.getMemberId(), educationListCreateRequest.getEducationList());
+        educationService.saveAll(accessor.getMemberId(), educationListCreateRequest.getEducationList());
         return ResponseEntity.ok().build();
     }
+
+    @PostMapping("/education")
+    @MemberOnly
+    public ResponseEntity<Void> createEducation(
+            @Auth final Accessor accessor,
+            @RequestBody @Valid EducationCreateRequest educationCreateRequest
+    ) {
+        log.info("memberId={}의 학력 생성 요청이 들어왔습니다.", accessor.getMemberId());
+        educationService.save(accessor.getMemberId(), educationCreateRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
 
     @DeleteMapping("/education/{educationId}")
     @MemberOnly
