61dfaa656358831b4cb8b2c16139c2dfc4135af9
oznchex <114240463+oznchex@users.noreply.github.com>
2024-07-14 18:45:03 +0900

    feat : 찜한 목록 조회하기 기능 추가

--- src/main/java/liaison/linkit/wish/presentation/WishController.java
+++ src/main/java/liaison/linkit/wish/presentation/WishController.java
@@ -3,15 +3,16 @@ package liaison.linkit.wish.presentation;
 import liaison.linkit.auth.Auth;
 import liaison.linkit.auth.MemberOnly;
 import liaison.linkit.auth.domain.Accessor;
+import liaison.linkit.wish.dto.response.MyWishResponse;
 import liaison.linkit.wish.service.WishService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WishController {
     public final WishService wishService;
 
+    // 내 이력서 찜하기
     @PostMapping("/wish/private/profile/{miniProfileId}")
     @MemberOnly
     public ResponseEntity<Void> createWishToPrivateProfile(
@@ -29,5 +31,28 @@ public class WishController {
         log.info("miniProfileId={} 을 memberId={}가 찜하는 요청이 발생했습니다.", miniProfileId, accessor.getMemberId());
         wishService.createWishToPrivateProfile(accessor.getMemberId(), miniProfileId);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    // 팀 소개서 찜하기
+    @PostMapping("/wish/team/profile/{teamMiniProfileId}")
+    @MemberOnly
+    public ResponseEntity<Void> createWishToTeamProfile(
+            @Auth final Accessor accessor,
+            @PathVariable final Long teamMiniProfileId
+    ) {
+        log.info("teamMiniProfileId={} 을 memberId={}가 찜하는 요청이 발생했습니다.", teamMiniProfileId, accessor.getMemberId());
+        wishService.createWishToTeamProfile(accessor.getMemberId(), teamMiniProfileId);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/my/wish/list")
+    @MemberOnly
+    public ResponseEntity<Page<MyWishResponse>> getMyWishList(
+            @Auth final Accessor accessor,
+            @PageableDefault(size = 10) Pageable pageable
+    ) {
+        log.info("memberId={}의 찜하기 목록을 가져옵니다.", accessor.getMemberId());
+        final Page<MyWishResponse> myWishResponsePage = wishService.getMyWishList(accessor.getMemberId(), pageable);
+        return ResponseEntity.ok(myWishResponsePage);
     }
 }
--- src/main/java/liaison/linkit/wish/service/WishService.java
+++ src/main/java/liaison/linkit/wish/service/WishService.java
@@ -2,22 +2,44 @@ package liaison.linkit.wish.service;
 
 import liaison.linkit.global.exception.BadRequestException;
 import liaison.linkit.member.domain.Member;
+import liaison.linkit.member.domain.MemberBasicInform;
+import liaison.linkit.member.domain.repository.MemberBasicInformRepository;
 import liaison.linkit.member.domain.repository.MemberRepository;
 import liaison.linkit.profile.domain.Profile;
 import liaison.linkit.profile.domain.miniProfile.MiniProfile;
+import liaison.linkit.profile.domain.miniProfile.MiniProfileKeyword;
+import liaison.linkit.profile.domain.repository.ProfileRepository;
+import liaison.linkit.profile.domain.repository.jobRole.ProfileJobRoleRepository;
+import liaison.linkit.profile.domain.repository.miniProfile.MiniProfileKeywordRepository;
 import liaison.linkit.profile.domain.repository.miniProfile.MiniProfileRepository;
+import liaison.linkit.profile.domain.role.JobRole;
+import liaison.linkit.profile.domain.role.ProfileJobRole;
+import liaison.linkit.profile.dto.response.miniProfile.MiniProfileResponse;
+import liaison.linkit.team.domain.TeamProfile;
+import liaison.linkit.team.domain.miniprofile.TeamMiniProfile;
+import liaison.linkit.team.domain.miniprofile.TeamMiniProfileKeyword;
+import liaison.linkit.team.domain.repository.miniprofile.TeamMiniProfileKeywordRepository;
+import liaison.linkit.team.domain.repository.miniprofile.TeamMiniProfileRepository;
+import liaison.linkit.team.dto.response.miniProfile.TeamMiniProfileResponse;
 import liaison.linkit.wish.domain.PrivateWish;
+import liaison.linkit.wish.domain.TeamWish;
 import liaison.linkit.wish.domain.repository.PrivateWishRepository;
+import liaison.linkit.wish.domain.repository.TeamWishRepository;
 import liaison.linkit.wish.domain.type.WishType;
+import liaison.linkit.wish.dto.response.MyWishResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
 
-import static liaison.linkit.global.exception.ExceptionCode.NOT_FOUND_MEMBER_BY_MEMBER_ID;
-import static liaison.linkit.global.exception.ExceptionCode.NOT_FOUND_MINI_PROFILE_BY_ID;
+import static liaison.linkit.global.exception.ExceptionCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -26,13 +48,26 @@ import static liaison.linkit.global.exception.ExceptionCode.NOT_FOUND_MINI_PROFI
 public class WishService {
 
     private final MemberRepository memberRepository;
+    private final ProfileRepository profileRepository;
     private final MiniProfileRepository miniProfileRepository;
+    private final TeamMiniProfileRepository teamMiniProfileRepository;
     private final PrivateWishRepository privateWishRepository;
+    private final TeamWishRepository teamWishRepository;
+    private final MemberBasicInformRepository memberBasicInformRepository;
+    private final ProfileJobRoleRepository profileJobRoleRepository;
+    private final MiniProfileKeywordRepository miniProfileKeywordRepository;
+    private final TeamMiniProfileKeywordRepository teamMiniProfileKeywordRepository;
 
     // 회원 정보를 가져오는 메서드
     private Member getMember(final Long memberId) {
         return memberRepository.findById(memberId)
                 .orElseThrow(() -> new BadRequestException(NOT_FOUND_MEMBER_BY_MEMBER_ID));
+    }
+
+    // 모든 "내 이력서" 서비스 계층에 필요한 profile 조회 메서드
+    private Profile getProfile(final Long memberId) {
+        return profileRepository.findByMemberId(memberId)
+                .orElseThrow(() -> new BadRequestException(NOT_FOUND_PROFILE_BY_MEMBER_ID));
     }
 
     private Profile getProfileByMiniProfileId(
@@ -42,6 +77,15 @@ public class WishService {
                 .orElseThrow(() -> new BadRequestException(NOT_FOUND_MINI_PROFILE_BY_ID));
 
         return miniProfile.getProfile();
+    }
+
+    private TeamProfile getTeamProfileByTeamMiniProfileId(
+            final Long teamMiniProfileId
+    ) {
+        final TeamMiniProfile teamMiniProfile = teamMiniProfileRepository.findById(teamMiniProfileId)
+                .orElseThrow(() -> new BadRequestException(NOT_FOUND_TEAM_MINI_PROFILE_ID));
+
+        return teamMiniProfile.getTeamProfile();
     }
 
     public void createWishToPrivateProfile(
@@ -63,4 +107,83 @@ public class WishService {
     }
 
 
+    public void createWishToTeamProfile(
+            final Long memberId,
+            final Long teamMiniProfileId
+    ) {
+        final Member member = getMember(memberId);
+        final TeamProfile teamProfile = getTeamProfileByTeamMiniProfileId(teamMiniProfileId);
+
+        final TeamWish teamWish = new TeamWish(
+                null,
+                member,
+                teamProfile,
+                WishType.TEAM_PROFILE,
+                LocalDateTime.now()
+        );
+
+        teamWishRepository.save(teamWish);
+    }
+
+    // 찜한 팀원, 찜한 팀 모두 반환 필요
+    public Page<MyWishResponse> getMyWishList(final Long memberId, final Pageable pageable) {
+        // 사용자 찾기
+        final Member member = getMember(memberId);
+
+        final List<PrivateWish> privateWishList = privateWishRepository.findAllByMemberId(member.getId());
+        final List<MiniProfileResponse> miniProfileList = privateWishList.stream()
+                .map(privateWish -> {
+                    final MiniProfile miniProfile = privateWish.getProfile().getMiniProfile();
+                    final List<MiniProfileKeyword> miniProfileKeywords = getMiniProfileKeywords(miniProfile.getId());
+                    final MemberBasicInform memberBasicInform = getMemberBasicInform(memberId);
+                    final List<String> jobRoleNames = getJobRoleNames(memberId);
+                    return MiniProfileResponse.personalMiniProfile(
+                            miniProfile,
+                            miniProfileKeywords,
+                            memberBasicInform.getMemberName(),
+                            jobRoleNames);
+                })
+                .toList();
+        // 팀 찜 목록
+        final List<TeamWish> teamWishList = teamWishRepository.findAllByMemberId(member.getId());
+        final List<TeamMiniProfileResponse> teamMiniProfileList = teamWishList.stream()
+                .map(teamWish -> {
+                    final TeamMiniProfile teamMiniProfile = teamWish.getTeamProfile().getTeamMiniProfile();
+                    final List<TeamMiniProfileKeyword> teamMiniProfileKeyword = teamMiniProfileKeywordRepository.findAllByTeamMiniProfileId(teamMiniProfile.getId());
+                    return TeamMiniProfileResponse.personalTeamMiniProfile(teamMiniProfile, teamMiniProfileKeyword);
+                })
+                .toList();
+
+        final MyWishResponse myWishResponse = new MyWishResponse(miniProfileList, teamMiniProfileList);
+        List<MyWishResponse> myWishResponseList = Collections.singletonList(myWishResponse);
+
+        return new PageImpl<>(myWishResponseList, pageable, myWishResponseList.size());
+    }
+
+    private List<MiniProfileKeyword> getMiniProfileKeywords(final Long miniProfileId) {
+        return miniProfileKeywordRepository.findAllByMiniProfileId(miniProfileId);
+    }
+
+    // 회원 기본 정보를 가져오는 메서드
+    private MemberBasicInform getMemberBasicInform(final Long memberId) {
+        return memberBasicInformRepository.findByMemberId(memberId)
+                .orElseThrow(() -> new BadRequestException(NOT_FOUND_MEMBER_BASIC_INFORM_BY_MEMBER_ID));
+    }
+
+    public List<String> getJobRoleNames(final Long memberId) {
+        final Profile profile = getProfile(memberId);
+        final List<ProfileJobRole> profileJobRoleList = getProfileJobRoleList(profile.getId());
+
+        List<JobRole> jobRoleList = profileJobRoleList.stream()
+                .map(ProfileJobRole::getJobRole)
+                .toList();
+
+        return jobRoleList.stream()
+                .map(JobRole::getJobRoleName)
+                .toList();
+    }
+
+    private List<ProfileJobRole> getProfileJobRoleList(final Long profileId) {
+        return profileJobRoleRepository.findAllByProfileId(profileId);
+    }
 }
