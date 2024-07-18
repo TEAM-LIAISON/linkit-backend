09fbaa4c6e05513bcf052d910ab7610f97b103ae
oznchex <114240463+oznchex@users.noreply.github.com>
2024-07-18 21:18:56 +0900

    fix : antecedents add

--- src/main/java/liaison/linkit/profile/dto/request/antecedents/AntecedentsCreateRequest.java
+++ src/main/java/liaison/linkit/profile/dto/request/antecedents/AntecedentsCreateRequest.java
@@ -14,17 +14,11 @@ public class AntecedentsCreateRequest {
     @NotNull(message = "직무(역할)을 입력해주세요")
     private final String projectRole;
 
-    @NotNull(message = "시작 연도를 입력해주세요")
-    private final int startYear;
+    @NotNull(message = "시작 연도/월을 입력해주세요")
+    private final String startDate;
 
-    @NotNull(message = "시작 월을 입력해주세요")
-    private final int startMonth;
-
-    @NotNull(message = "종료 연도를 입력해주세요")
-    private final int endYear;
-
-    @NotNull(message = "종료 월을 입력해주세요")
-    private final int endMonth;
+    @NotNull(message = "종료 연도/월을 입력해주세요")
+    private final String endDate;
 
     @NotNull(message = "재직 여부를 선택해주세요")
     private final boolean retirement;
