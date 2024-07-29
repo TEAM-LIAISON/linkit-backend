10c06ce5bad283f4f49a2acb67d1524bc1f03a50
oznchex <114240463+oznchex@users.noreply.github.com>
2024-07-29 20:38:03 +0900

    feat : mail auto send 구현

---
+++ src/main/java/liaison/linkit/mail/service/MailService.java
@@ -0,0 +1,9 @@
+package liaison.linkit.mail.service;
+
+public interface MailService {
+    // 메일 발송
+    String sendSimpleMessage(String to)throws Exception;
+
+    // 검증
+    String verifyCode(String code);
+}
