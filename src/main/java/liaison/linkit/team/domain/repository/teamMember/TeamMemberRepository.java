a075ad8ffb6af7bdc47ca633a6ea212a99db9c2a
oznchex <114240463+oznchex@users.noreply.github.com>
2024-09-05 12:40:01 +0900

    feat : Team & Team Member Repository 추가

---
+++ src/main/java/liaison/linkit/team/domain/repository/teamMember/TeamMemberRepository.java
@@ -0,0 +1,8 @@
+package liaison.linkit.team.domain.repository.teamMember;
+
+import liaison.linkit.team.domain.TeamMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamMemberRepository extends JpaRepository<TeamMember, Long>, TeamMemberRepositoryCustom {
+
+}
