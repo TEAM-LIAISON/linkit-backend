0f0a2259f6639f50694eafba394c8fbeb76117e8
oznchex <114240463+oznchex@users.noreply.github.com>
2024-11-04 00:43:12 +0900

    feat: application yml 추가

--- src/main/java/liaison/linkit/global/config/CorsConfig.java
+++ src/main/java/liaison/linkit/global/config/CorsConfig.java
@@ -1,23 +1,24 @@
 package liaison.linkit.global.config;
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${cors-allowed-origins}")
+    private List<String> corsAllowedOrigins;
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:3000",
-                        "https://www.linkit.im",
-                        "https://linkit.im",
-                        "https://dev-linkit.vercel.app",
-                        "https://www.liaison-dev.site",
-                        "https://dev.liaison-dev.site")
+                .allowedOrigins(corsAllowedOrigins.toArray(new String[0]))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowCredentials(true)
                 .exposedHeaders(HttpHeaders.LOCATION);
