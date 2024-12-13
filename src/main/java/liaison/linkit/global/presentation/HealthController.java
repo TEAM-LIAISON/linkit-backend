02eb271ae2e291d306e48cd163a64fd233df2987
oznchex <114240463+oznchex@users.noreply.github.com>
2024-12-13 14:16:38 +0900

    fix: local common blue green 설정 변경

--- src/main/java/liaison/linkit/global/presentation/HealthController.java
+++ src/main/java/liaison/linkit/global/presentation/HealthController.java
@@ -5,12 +5,14 @@ import java.util.TreeMap;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
+@Component
 public class HealthController {
 
 
