017dfd63773df72b3420a72d4f3f391d544104be
oznchex <114240463+oznchex@users.noreply.github.com>
2024-12-13 14:33:17 +0900

    fix: error 관련 설정 일정 부분 제거

--- src/main/java/liaison/linkit/global/presentation/HealthController.java
+++ src/main/java/liaison/linkit/global/presentation/HealthController.java
@@ -2,40 +2,25 @@ package liaison.linkit.global.presentation;
 
 import java.util.Map;
 import java.util.TreeMap;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-@Component
 public class HealthController {
 
 
     @Value("${server.env}")
     private String env;
 
-    @Value("${server.port}")
-    private String serverPort;
-
-    @Value("${server.serverAddress}")
-    private String address;
-
-    @Value("${serverName}")
-    private String serverName;
-
 
     @GetMapping("/hc")
     public ResponseEntity<?> healthcheck() {
         Map<String, String> responseData = new TreeMap<>();
-
-        responseData.put("serverName", serverName);
-        responseData.put("serverAddress", address);
-        responseData.put("serverPort", serverPort);
+        
         responseData.put("env", env);
 
         return ResponseEntity.ok(responseData);
