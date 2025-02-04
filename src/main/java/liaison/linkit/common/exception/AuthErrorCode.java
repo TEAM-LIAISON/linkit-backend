9b7186b1f0eb77c7b8777b30d2823e171be366e5
oznchex <114240463+oznchex@users.noreply.github.com>
2025-02-04 11:27:45 +0900

    fix: 엑세스 토큰 만ㄹ 411로 수정

--- src/main/java/liaison/linkit/common/exception/AuthErrorCode.java
+++ src/main/java/liaison/linkit/common/exception/AuthErrorCode.java
@@ -13,11 +13,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum AuthErrorCode implements BaseErrorCode {
 
-    ACCESS_TOKEN_EXPIRED(450, "AUTH_450_1", "AccessToken 인증 시간이 만료되었습니다. 인증토큰을 재발급 해주세요"),
-    INVALID_ACCESS_TOKEN(450, "AUTH_450_2", "유효하지 않은 AccessToken입니다. 인증토큰을 재발급 해주세요"),
+    ACCESS_TOKEN_EXPIRED(411, "AUTH_450_1", "AccessToken 인증 시간이 만료되었습니다. 인증토큰을 재발급 해주세요"),
+    INVALID_ACCESS_TOKEN(411, "AUTH_450_2", "유효하지 않은 AccessToken입니다. 인증토큰을 재발급 해주세요"),
     INVALID_REFRESH_TOKEN(FORBIDDEN, "AUTH_403_3", "유효하지 않은 RefreshToken입니다. 재로그인 해주세요"),
     REFRESH_TOKEN_EXPIRED(FORBIDDEN, "AUTH_403_1", "RefreshToken 인증 시간이 만료되었습니다. 재로그인 해주세요."),
-    ACCESS_TOKEN_NOT_EXIST(450, "AUTH_450_2", "올바른 AccessToken을 넣어주세요."),
+    ACCESS_TOKEN_NOT_EXIST(411, "AUTH_450_2", "올바른 AccessToken을 넣어주세요."),
     NOT_SUPPORTED_OAUTH_SERVICE(INTERNAL_SERVER, "AUTH_500_1", "OAUTH 서비스에 문제가 생겨 지원되지 않습니다.");
 
     private final Integer status;
