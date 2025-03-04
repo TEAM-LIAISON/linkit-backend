261a4d1e06f08aacbbb1f118a6af790458bb53d8
oznchex <114240463+oznchex@users.noreply.github.com>
2025-03-04 20:12:54 +0900

    fix: swagger 설정 제거

--- src/main/java/liaison/linkit/global/config/swagger/ScanningPropertiesConfig.java
+++
@@ -1,8 +0,0 @@
-package liaison.linkit.global.config.swagger;
-
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ConfigurationPropertiesScan(basePackages = "liaison.linkit")
-public class ScanningPropertiesConfig {}
--- src/main/java/liaison/linkit/global/config/swagger/SwaggerProperties.java
+++
@@ -1,6 +0,0 @@
-package liaison.linkit.global.config.swagger;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "swagger")
-public record SwaggerProperties(String version) {}
