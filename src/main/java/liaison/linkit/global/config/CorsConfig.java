ca75efc83e2f11cebcf454dd54f2141620a4da02
oznchex <114240463+oznchex@users.noreply.github.com>
2025-03-12 19:03:48 +0900

    fix: CorsConfig 클래스에 인터셉터를 등록

--- src/main/java/liaison/linkit/global/config/CorsConfig.java
+++ src/main/java/liaison/linkit/global/config/CorsConfig.java
@@ -1,14 +1,18 @@
 package liaison.linkit.global.config;
 
+import liaison.linkit.login.infrastructure.AuthTokenMigrationInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
 public class CorsConfig implements WebMvcConfigurer {
+
+    private final AuthTokenMigrationInterceptor authTokenMigrationInterceptor;
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
@@ -20,9 +24,14 @@ public class CorsConfig implements WebMvcConfigurer {
                         "https://www.linkit.im",
                         "https://linkit.im")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                .allowCredentials(true)
-                .exposedHeaders(HttpHeaders.LOCATION);
+                .allowedHeaders("*") // 모든 요청 헤더 허용
+                .allowCredentials(true) // 쿠키 허용 (필수)
+                .exposedHeaders(HttpHeaders.LOCATION, HttpHeaders.SET_COOKIE); // Set-Cookie 헤더 노출 추가
+    }
 
-        WebMvcConfigurer.super.addCorsMappings(registry);
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authTokenMigrationInterceptor)
+                .addPathPatterns("/api/**"); // 모든 API에 적용
     }
 }
